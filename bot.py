379d913056aa896816f9dda25a16069304a5c1c2
Ali-Maghari <aliplaywork@gmail.com>
2023-06-05 13:16:14 +0300

    Some fixes 2

--- bot.py
+++ bot.py
@@ -14,36 +14,46 @@ updater = Updater(token=TOKEN)
 # Get the dispatcher to register handlers
 dispatcher = updater.dispatcher
 
-# Define the command for adding a word to the delete list
+admin_username = 'Ali_J_Maghari'
+pre_selected_username = 'Ali_J_Maghari'
+words_to_delete = []  # Add the words you want to delete messages for
+words_to_reply = ['كلمة ٥', 'كلمة ٦']  # Add the words you want to reply to
+
+# Define the command for adding a word to the delete list and check if the user is admin
 def add_delete_word(update, context):
-    if len(context.args) > 0:
-        new_word = ' '.join(context.args)
-        # Add the new word to the delete list
-        context.user_data.setdefault('delete_words', []).append(new_word)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"'{new_word}' added to delete list.")
+    if update.message.from_user.username == admin_username:
+        if len(context.args) > 0:
+            new_word = ' '.join(context.args)
+            # Add the new word to the delete list
+            context.user_data.setdefault('delete_words', []).append(new_word)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"'{new_word}' added to delete list.")
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a word to add to the delete list.")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a word to add to the delete list.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not authorized to use this command.")
 
 add_delete_word_handler = CommandHandler('add_delete_word', add_delete_word)
 dispatcher.add_handler(add_delete_word_handler)
 
-# Define the command for removing a word from the delete list
+# Define the command for removing a word from the delete list and check if the user is admin
 def remove_delete_word(update, context):
-    if len(context.args) > 0:
-        word_to_remove = ' '.join(context.args)
-        delete_words = context.user_data.get('delete_words', [])
-        if word_to_remove in delete_words:
-            # Remove the word from the delete list
-            delete_words.remove(word_to_remove)
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"'{word_to_remove}' removed from delete list.")
+    if update.message.from_user.username == admin_username:
+        if len(context.args) > 0:
+            word_to_remove = ' '.join(context.args)
+            delete_words = context.user_data.get('delete_words', [])
+            if word_to_remove in delete_words:
+                # Remove the word from the delete list
+                delete_words.remove(word_to_remove)
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"'{word_to_remove}' removed from delete list.")
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"'{word_to_remove}' is not in the delete list.")
         else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"'{word_to_remove}' is not in the delete list.")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a word to remove from the delete list.")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a word to remove from the delete list.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not authorized to use this command.")
 
 remove_delete_word_handler = CommandHandler('remove_delete_word', remove_delete_word)
 dispatcher.add_handler(remove_delete_word_handler)
-
 
 # Define the command for adding the bot to a group
 def start(update, context):
@@ -59,10 +69,6 @@ def send_reply(update, context):
 
 # Define the function to delete messages containing specific words
 def delete_message(update, context):
-    pre_selected_username = 'Ali_J_Maghari'
-    words_to_delete = []  # Add the words you want to delete messages for
-    words_to_reply = ['كلمة ٥', 'كلمة ٦']  # Add the words you want to reply to
-
     message_text = update.message.text.lower()
     if update.message.from_user.username == pre_selected_username:
         for word in words_to_reply:
